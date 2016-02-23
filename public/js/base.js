39cbdf36d274e76169e0d61eba369abedaa41c87
DasonCheng <dasoncheng@foxmail.com>
2016-02-23 23:15:07 +0800

    Version 1.0.0

--- public/js/base.js
+++
@@ -1,5 +0,0 @@
-$(function(){
-    $('.navbar-inverse .navbar-nav>li').click(function(){
-        $(this).addClass('active').siblings().removeClass('active');
-    })
-})
--- router/router.js
+++ router/router.js
@@ -11,22 +11,26 @@ router.use(function timeLog(req, res, next) {
 });
 router.get('/', function(req, res) {
     res.render('index',{
-        title:'首页'
+        title:'首页',
+        current:'/'
     });
 });
 router.get('/case', function(req, res) {
     res.render('case',{
-        title:'案例'
+        title:'案例',
+        current:'/case'
     });
 });
 router.get('/service', function(req, res) {
     res.render('service',{
-        title:'服务'
+        title:'服务',
+        current:'/service'
     });
 });
 router.get('/about', function(req, res) {
     res.render('about',{
-        title:'关于'
+        title:'关于',
+        current:'/about'
     });
 });
 router.get('/fullpage', function(req, res) {
